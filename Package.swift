b1920473395779aa3babe73f9343f5ef578cabaa
3F3Feng <43562203+3F3Feng@users.noreply.github.com>
2026-02-20 04:39:58 -0500

    Migrate to Swift Package Manager
    - Add Package.swift for SPM support
    - Move Sources to Sources/WolkieTalkie
    - Move Tests to Tests/WolkieTalkieTests
    - Update CI to use swift build/test
    - Remove Xcode project dependency

--- Package.swift
+++ Package.swift
@@ -1,25 +1,29 @@
 // swift-tools-version:5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "WolkieTalkie",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v16)
     ],
     products: [
-        .executable(
+        .library(
             name: "WolkieTalkie",
             targets: ["WolkieTalkie"]
-        )
+        ),
     ],
     dependencies: [],
     targets: [
-        .executableTarget(
+        .target(
             name: "WolkieTalkie",
             dependencies: [],
-            path: "Sources"
-        )
+            path: "Sources/WolkieTalkie",
+            exclude: ["ProximityManager_fixes.swift"]
+        ),
+        .testTarget(
+            name: "WolkieTalkieTests",
+            dependencies: ["WolkieTalkie"],
+            path: "Tests/WolkieTalkieTests"
+        ),
     ]
 )
